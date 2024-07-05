4c7d79b7430d23f053bddb173d9833641489b334
Walter kibet <kibetwalter726@gmail.com>
2024-07-05 16:08:31 +0300

    update on views

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -15,14 +16,14 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date = 'publish')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name = 'blog_posts'
     )
     body = models.TextField()
-    publish=models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
@@ -33,6 +34,7 @@ class Post(models.Model):
 
     objects= models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
@@ -58,7 +60,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') 
     name = models.CharField(max_length=80)
     email = models.EmailField()
-    body=models.TextField()
+    body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
--- app/views.py
+++ app/views.py
@@ -1,14 +1,22 @@
+
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.shortcuts import render,get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from .forms import CommentForm, EmailPostForm
+from taggit.models import Tag
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import *
 # Create your views here.
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -20,7 +28,14 @@ def post_list(request):
     except EmptyPage:
         # If page_number is out of range get last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(
+        request, 
+        'blog/post/list.html', 
+        {
+            'posts': posts,
+            'tag': tag
+        }
+    )
 
 
 
@@ -37,14 +52,27 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post_id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+
+
+
     return render(
         request, 
         'blog/post/detail.html', 
         {
             'post': post,
             'comments': comments,
-            'form': form
-        }
+            'form': form,
+            'similar_posts': similar_posts,
+        },
     )
 
 class PostListView(ListView):
@@ -97,5 +125,58 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent
+        },
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post =get_object_or_404(
+        Post,
+        id=post_id,
+        status = Post.Status.PUBLISHED
+    )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        },
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    similarity=TrigramSimilarity('title', query),
+                )
+                .filter(similarity__gt=0.1)
+                .order_by('-similarity')
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         },
     )
